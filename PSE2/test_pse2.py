9293479f957cb461a4557031ab5c04e753688127
Jenny <jfperham@gmail.com>
2022-05-17 21:10:25 -0700

    fixed tests

--- PSE2/test_pse2.py
+++ PSE2/test_pse2.py
@@ -1,17 +1,17 @@
 from pse2 import score, letter_dict
 
-def test_score_simple_word(word):
+def test_score_simple_word():
     # arrange
     word = "word"
     # act
     output = score(word)
     # assert
-    output == 8
+    assert output == 8
     
-def test_score_word_with_extra_characters(word):
+def test_score_word_with_extra_characters():
     # arrange
     word = "Hello, world!"
     # act
     output = score(word)
     # assert
-    output == 17
+    assert output == 17
